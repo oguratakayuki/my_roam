3f1563583aa02f6b773c993a7611e6681ab6f7b0
oguratakayuki <otn.ogura@gmail.com>
2014-01-20 16:06:02 +0900

    server side multi thread

--- server/game_tcp_server.rb
+++ server/game_tcp_server.rb
@@ -34,35 +34,35 @@ class GameTcpServer
     # 新しいサーバ接続をポート10001で開く
     server = TCPServer.open(@port=10005)
     # クライアントからの接続を待つ
-    loop do
+    while true
       # クライアントからの入力を出力(1行のみ)
-      client = server.accept
-      sock_domain, remote_port, remote_hostname, remote_ip = client.peeraddr
-      message = client.gets
-      puts message
-      next if message == nil
-      puts 'server accept ok'
-      message = message.chomp
-      message = JSON.parse(message)
-      if message['cmd'] == 'new_user_id'
-        @logger.error 'new_user_id accepted'
-        puts "accept from #{remote_ip} ok"
-        user_id = @user_list.get_new_user_by_ip(remote_ip, '10004')
-        result = {:user_id => user_id}.to_json
-        client.puts result
-      elsif message['cmd'] == 'move'
-        @logger.error 'move accepted'
-        @user_list.update_by_id(message['params']['user_id'], message['params']['x'], message['params']['y'])
-        send_message_to_all_client('update_all_user_position', @user_list.ips_and_ports, @user_list.positions)
-      else
-        @logger.error "else message #{message.to_s}"
+      Thread.start(server.accept) do |sock|
+        sock_domain, remote_port, remote_hostname, remote_ip = sock.peeraddr
+        puts "sock_domain=#{sock_domain.to_s},remote_port=#{remote_port.to_s},remote_hostname=#{remote_hostname.to_s},remote_ip=#{remote_ip.to_s}"
+        message = sock.gets
         puts message
-        puts 'unknown'
+        next if message == nil
+        puts 'server accept ok'
+        message = message.chomp
+        message = JSON.parse(message)
+        if message['cmd'] == 'new_user_id'
+          @logger.error 'new_user_id accepted'
+          puts "accept from #{remote_ip} ok"
+          user_id = @user_list.get_new_user_by_ip(remote_ip, '10004')
+          result = {:user_id => user_id}.to_json
+          sock.puts result
+        elsif message['cmd'] == 'move'
+          @logger.error 'move accepted'
+          @user_list.update_by_id(message['params']['user_id'], message['params']['x'], message['params']['y'])
+          send_message_to_all_client('update_all_user_position', @user_list.ips_and_ports, @user_list.positions)
+        else
+          @logger.error "else message #{message.to_s}"
+          puts message
+          puts 'unknown'
+        end
+        sock.close
+        puts 'hrer'
       end
-      #sleep 3
-      puts 'hrer'
-      # クライアントの接続を閉じる
-      #client.close
     end
   end
 end
